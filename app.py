d8693c38839cee4633565c7c73ae7c9897a8be09
TheFrostKing <svetlinmail@gmail.com>
2022-04-08 16:56:15 +0200

    only important code is left

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # import packages
 from email.mime import application
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from models import db, Application_Error
  
@@ -31,7 +31,7 @@ def create():
         application_log = Application_Error(level=level, date=date_time, source=source, event_id=event_id)
         db.session.add(application_log)
         db.session.commit()
-        return redirect('/data')
+        return redirect(url_for('data'))
     
     
 
@@ -42,34 +42,34 @@ def RetrieveList():
     return render_template('datalist.html',application_logs = application_logs)
 
 
-@app.route('/data/<str:id>') # SOLO SEARCH
-def RetrieveLog(id):
-    application_log = Application_Error.query.filter_by(level=id).first()
-    if application_log:
-        return render_template('data.html', employee = application_log)
-    return f"Employee with id ={id} Doesn't exist"
+# @app.route('/data/<str:id>') # SOLO SEARCH
+# def RetrieveLog(id):
+#     application_log = Application_Error.query.filter_by(level=id).first()
+#     if application_log:
+#         return render_template('data.html', employee = application_log)
+#     return f"Employee with id ={id} Doesn't exist"
  
 
-@app.route('/data/<string:level>/update',methods = ['GET','POST'])
-def update(id):
-    application_log = Application_Error.query.filter_by(level=id).first()
-    if request.method == 'POST':
-        if application_log:
-            db.session.delete(application_log)
-            db.session.commit()
+# @app.route('/data/<string:level>/update',methods = ['GET','POST'])
+# def update(id):
+#     application_log = Application_Error.query.filter_by(level=id).first()
+#     if request.method == 'POST':
+#         if application_log:
+#             db.session.delete(application_log)
+#             db.session.commit()
  
-            level = request.form['level']
-            date_time = request.form['date_time']
-            source = request.form['source']
-            event_id = request.form['event_id']
-            application_log = Application_Error(level=level, date=date_time, source=source, event_id = event_id)
+#             level = request.form['level']
+#             date_time = request.form['date_time']
+#             source = request.form['source']
+#             event_id = request.form['event_id']
+#             application_log = Application_Error(level=level, date=date_time, source=source, event_id = event_id)
  
-            db.session.add(application_log)
-            db.session.commit()
-            return redirect(f'/data/{id}')
-        return f"Employee with id = {id} Does not exist"
+#             db.session.add(application_log)
+#             db.session.commit()
+#             return redirect(f'/data/{id}')
+#         return f"Employee with id = {id} Does not exist"
  
-    return render_template('update.html', application_log = application_log)
+#     return render_template('update.html', application_log = application_log)
 
  
 app.run(host='localhost', port=5000)
